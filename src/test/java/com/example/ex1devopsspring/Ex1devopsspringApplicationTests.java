5f9c29d94cbe9d0f8e5f583d15312f8b6f2f31c2
hatem <hlajili13@gmail.com>
2021-12-25 15:52:53 +0100

    first commit

---
+++ src/test/java/com/example/ex1devopsspring/Ex1devopsspringApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.ex1devopsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootTest
+@EnableSwagger2
+class Ex1devopsspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
